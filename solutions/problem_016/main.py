b157a2389b5d76d2d2996be74ff58176084e834b
jsmithdataanalytics <jsmithdataanalytics@gmail.com>
2020-01-01 10:06:23 +0000

    Problem 16 solved

---
+++ solutions/problem_016/main.py
@@ -0,0 +1,40 @@
+
+class Queue(dict):
+
+    def __init__(self, max_length):
+        super().__init__(self)
+        self.max_length = max_length
+        self.first = 0
+        self.last = 0
+
+    def record(self, order_id):
+
+        if self:
+            self.last += 1
+
+            if len(self) == self.max_length:
+                del self[self.first]
+                self.first += 1
+
+        self[self.last] = order_id
+
+    def get_last(self, i):
+        return self[self.last - i + 1]
+
+
+if __name__ == '__main__':
+    q = Queue(max_length=3)
+    q.record('a')
+    q.record('b')
+    q.record('c')
+    q.record('d')
+
+    assert q.get_last(1) == 'd'
+    assert q.get_last(2) == 'c'
+    assert q.get_last(3) == 'b'
+
+    try:
+        q.get_last(4)
+
+    except KeyError:
+        print('Success!')
